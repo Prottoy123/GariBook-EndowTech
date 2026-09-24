31a5ef8b99d33944566bc4b8e782e394d3508dd9
nirob <nirobprottoy12@gmail.com>
2026-09-25 05:57:41 +0600

    feat: add main App component and web asset inspection scripts

--- src/App.jsx
+++ src/App.jsx
@@ -1,13 +1,32 @@
-import React from 'react';
+import React, { useEffect } from 'react';
+import AOS from 'aos';
+import 'aos/dist/aos.css';
 import Navbar from './components/layout/Navbar';
 import Hero from './components/hero/Hero';
 import Stats from './components/sections/Stats';
 import ServicesGrid from './components/sections/ServicesGrid';
-import PromoSections from './components/sections/PromoSections';
-import Features from './components/sections/Features';
+import FreedomJourney from './components/sections/FreedomJourney';
+import PeopleTogether from './components/sections/PeopleTogether';
+import BookingArrival from './components/sections/BookingArrival';
+import SmartDriver from './components/sections/SmartDriver';
+import FeaturedNews from './components/sections/FeaturedNews';
+import PassengerSpeak from './components/sections/PassengerSpeak';
+import BeyondDestinations from './components/sections/BeyondDestinations';
+import DownloadApp from './components/sections/DownloadApp';
 import Footer from './components/layout/Footer';
+import FloatingWidgets from './components/layout/FloatingWidgets';
 
 function App() {
+  useEffect(() => {
+    AOS.init({
+      duration: 800,
+      once: false,
+      mirror: false,
+      offset: 100,
+      easing: 'ease-out-cubic',
+    });
+  }, []);
+
   return (
     <div className="font-sans antialiased bg-white text-[#121212]">
       <Navbar />
@@ -15,10 +34,17 @@ function App() {
         <Hero />
         <Stats />
         <ServicesGrid />
-        <PromoSections />
-        <Features />
+        <FreedomJourney />
+        <PeopleTogether />
+        <BookingArrival />
+        <SmartDriver />
+        <FeaturedNews />
+        <PassengerSpeak />
+        <BeyondDestinations />
+        <DownloadApp />
       </main>
       <Footer />
+      <FloatingWidgets />
     </div>
   );
 }
